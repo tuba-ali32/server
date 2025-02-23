fa4487bba3b401d56da83a71c5da08463f5eb6d2
tuba-ali32 <tubaali3167@gmail.com>
2025-02-23 21:48:50 +0500

    changed

--- app.mjs
+++ app.mjs
@@ -1,11 +1,29 @@
 import express from "express"
 const app = express();
-const port =process.env.PORT||3000;
+const port = process.env.PORT || 5000;
+import path from "path"	
 
-app.get('/', (req, res) => {
+app.get('/abc', (req, res) => {
 console.log("request ip:" ,req.ip)
 	res.send('Hello from server!'+new Date().toString());
 });
+
+
+app.get('/weather/:cityName/:side', (req, res) => {
+	console.log(req.params)
+	console.log('request ip:', req.ip);
+	res.send('Hello from server!' + new Date().toString());
+});
+app.get('/weather/:cityName', (req, res) => {
+	console.log(req.params);
+	console.log('request ip:', req.ip);
+	res.send("hjk");
+});
+
+const __dirname = path.resolve();
+
+app.use('/', express.static(path.join(__dirname, './web/dist')));
+
 
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
